e88b69e22d46fb86e1c45421acad9163a0d19e26
kirillgordeev <gordey_89@mail.ru>
2025-11-11 21:39:22 +0300

    renamve config port to server port

--- src/app_config.rs
+++ src/app_config.rs
@@ -4,14 +4,14 @@ use std::env;
 #[derive(Debug)]
 pub struct AppConfig {
     db_url: String,
-    port: u16,
+    server_port: u16,
 }
 
 impl AppConfig {
     pub fn from_env() -> Result<Self> {
         Ok(Self {
             db_url: env::var("DATABASE_URL").context("DATABASE_URL must be set")?,
-            port: env::var("SERVER_PORT")
+            server_port: env::var("SERVER_PORT")
                 .unwrap_or_else(|_| "3000".to_string())
                 .parse()?,
         })
@@ -21,7 +21,7 @@ impl AppConfig {
         &self.db_url
     }
 
-    pub fn port(&self) -> &u16 {
-        &self.port
+    pub fn server_port(&self) -> &u16 {
+        &self.server_port
     }
 }
