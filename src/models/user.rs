430e782c31ed3548bb0b0d971e0aabe2598053c8
kirillgordeev <gordey_89@mail.ru>
2025-11-18 18:31:03 +0300

    added token model, user validation schemas

--- src/models/user.rs
+++ src/models/user.rs
@@ -15,3 +15,23 @@ pub struct User {
     pub created_at: Option<DateTime<Utc>>,
     pub updated_at: Option<DateTime<Utc>>,
 }
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct TokenClaims {
+    pub sub: String,
+    pub iat: usize,
+    pub exp: usize,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct RegisterUserSchema {
+    pub name: String,
+    pub email: String,
+    pub password: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct LoginUserSchema {
+    pub email: String,
+    pub password: String,
+}
